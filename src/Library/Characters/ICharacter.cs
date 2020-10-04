fdbfd2ea6b573d03965879a101dcb9ad9cc70225
RodrigoCD16 <53954034+RodrigoCD16@users.noreply.github.com>
2020-10-04 20:04:22 -0300

    Spellbook y comentarios
    Arreglos en Spellbook para los nuevos estandares y añadido de comentrarios en las interfaces y algunas clases

--- src/Library/Characters/ICharacter.cs
+++ src/Library/Characters/ICharacter.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 
 namespace RoleplayGame
 {
+    /*Interfaz que sirve de molde para todo personaje del tipo ICharacter con todas las cosas que tienen en común*/
     public interface ICharacter
     {
         int Health { get; set; }
--- src/Library/Characters/IItem.cs
+++ src/Library/Characters/IItem.cs
@@ -1,5 +1,6 @@
 namespace RoleplayGame
-{
+{   
+    /*Llegamos a esta interfaz al boservar que todos los items compartían estas bases*/
     public interface IItem
     {
         int AttackValue { get; }
